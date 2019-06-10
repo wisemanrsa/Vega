f62dfdeb36887fd4e07d565b6499e1da0513b770
tebogoma <tebogoma@vatit.com>
2019-06-10 23:37:17 +0200

    Seeded the database

---
+++ Migrations/20190610213107_SeedFeatures.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vega_be.Migrations
+{
+    public partial class SeedFeatures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            GetFeatures().ForEach(feature => migrationBuilder.Sql($"INSERT INTO FEATURES(Name) VALUES('{feature}')"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            GetFeatures().ForEach(feature => migrationBuilder.Sql($"DELETE FROM FEATURES WHERE Name = '{feature}'"));
+        }
+
+        protected List<string> GetFeatures()
+        {
+            return new List<string>() { "USB", "CD", "AC", "Sunroof", "Heated Seats", "Electric Windows" };
+        }
+    }
+}
